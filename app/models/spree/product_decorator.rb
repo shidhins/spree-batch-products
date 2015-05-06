f3e3d883c2a4410e73ff2172e658aaa6ef9d6f3f
Roman Smirnov <POMAHC@gmail.com>
2015-05-06 12:21:36 +0300

    Avoid double loading the same products because of joins

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,4 +1,7 @@
 Spree::Product.class_eval do
-  scope :for_backup, -> {includes(:master).active}
+  scope :for_backup, -> {
+    preload(:master => :prices).
+    where("#{quoted_table_name}.available_on <= ?", Time.now)
+  }
   FIELDS_FOR_BACKUP = ['sku', 'name', 'price', 'weight', 'description', 'slug']
 end
